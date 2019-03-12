36bf8cbf5f81c80961c4cef2f21af6a80b3ac57c
Mister-maker <mkaggrawal97@gmail.com>
2019-03-12 23:37:51 +0530

    Registration form added

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 class RegistrationForm(models.Model):
-    text = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
+    student_name = models.CharField(max_length=200)
+    course = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    roll_number = models.IntegerField()
+    linkedin_url = models.CharField(max_length=200, blank=True)
+    github_url = models.CharField(max_length=200, blank=True)
+    portfolio_url = models.CharField(max_length=200, blank=True)
+    date_of_birth = models.CharField(max_length=100)
+    resume = models.CharField(max_length=200, blank=True)
+    college_company_name = models.CharField(max_length=200)
+    year = models.CharField(max_length=100)
+    internship_details = models.TextField(blank=True)
     def __str__(self):
-        return self.name
+        return self.student_name
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -8,14 +8,26 @@ from django.contrib import messages
 
 def registration_view(request, *args, **kwargs):
     if request.method == 'POST':
-        text = request.POST['text']
-        name = request.POST['name']
+        student_name = request.POST['student_name']
+        course = request.POST['course']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        roll_number = request.POST['roll_number']
+        linkedin_url = request.POST['linkedin_url']
+        github_url = request.POST['github_url']
+        portfolio_url = request.POST['portfolio_url']
+        date_of_birth = request.POST['date_of_birth']
+        resume = request.POST['resume']
+        college_company_name = request.POST['college_company_name']
+        year = request.POST['year']
+        internship_details = request.POST['internship_details']
+        
         #  Check if user has made inquiry already
-        if RegistrationForm.objects.filter(name=name).exists():
+        if RegistrationForm.objects.all().filter(roll_number=roll_number).exists():
             messages.error(request, 'You have already Registered ')
             return redirect('/accounts/register')
 
-        registrationForm = RegistrationForm(name=name, text=text)
+        registrationForm = RegistrationForm(student_name=student_name, course=course, email=email, phone=phone, roll_number=roll_number,linkedin_url=linkedin_url, github_url=github_url, portfolio_url=portfolio_url, date_of_birth=date_of_birth, resume=resume, college_company_name=college_company_name, year=year, internship_details=internship_details)
 
         registrationForm.save()
         messages.error(request, 'You have registered successfully')
