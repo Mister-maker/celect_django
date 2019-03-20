92efa9b54ac3eca40026cad58dc554ae3a03c743
Mister-maker <mkaggrawal97@gmail.com>
2019-03-20 14:06:28 +0530

    Team Updated

--- src/team/models.py
+++ src/team/models.py
@@ -5,8 +5,8 @@ class Team(models.Model):
   Post = models.CharField(max_length=200)
   description = models.TextField(blank=True)
   photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
-  linkedin = models.CharField(max_length=200)
-  facebook = models.CharField(max_length=200)
+  linkedin = models.CharField(max_length=200, blank=True)
+  twitter = models.CharField(max_length=200, blank=True)
   is_published = models.BooleanField(default=True)
   def __str__(self):
     return self.Name
